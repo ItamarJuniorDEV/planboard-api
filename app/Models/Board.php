3a9710c8922d0f12d7722293235f040707c3dc27
Itamar Junior <itamar.junior@irriga.net>
2026-03-14 19:00:48 -0300

    feat: migration e model de Boards

---
+++ app/Models/Board.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Board extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'name',
+        'status',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function columns()
+    {
+        return $this->hasMany(Column::class);
+    }
+}
