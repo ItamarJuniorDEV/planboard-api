fb120c9ea6160ab464182bf3a5f9b9077e0515cd
Itamar Junior <itamar.junior@irriga.net>
2026-03-14 18:59:35 -0300

    feat: migration e model de Projects

---
+++ app/Models/Project.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'budget',
+        'status',
+        'deadline',
+    ];
+
+    protected $casts = [
+        'budget' => 'decimal:2',
+        'deadline' => 'date',
+    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function boards()
+    {
+        return $this->hasMany(Board::class);
+    }
+
+    public function milestones()
+    {
+        return $this->hasMany(Milestone::class);
+    }
+
+    public function labels()
+    {
+        return $this->hasMany(Label::class);
+    }
+}
