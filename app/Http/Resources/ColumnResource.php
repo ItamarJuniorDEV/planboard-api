54d4332c7eecf9ec0fa67c42119c0d8ceda177df
Itamar Junior <100168621+ItamarJuniorDEV@users.noreply.github.com>
2026-08-22 14:57:04 -0300

    refactor: tipar resources da api

--- app/Http/Resources/ColumnResource.php
+++ app/Http/Resources/ColumnResource.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Column;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/** @mixin Column */
 class ColumnResource extends JsonResource
 {
+    /** @return array<string, mixed> */
     public function toArray(Request $request): array
     {
         return [
--- app/Http/Resources/MilestoneResource.php
+++ app/Http/Resources/MilestoneResource.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Milestone;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/** @mixin Milestone */
 class MilestoneResource extends JsonResource
 {
+    /** @return array<string, mixed> */
     public function toArray(Request $request): array
     {
         return [
